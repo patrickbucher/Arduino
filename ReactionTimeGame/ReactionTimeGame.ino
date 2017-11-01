84ba4c659c12ca108583d8070e699172e45f4515
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2017-11-01 23:36:11 +0100

    store top results in reaction time game

--- ReactionTimeGame/ReactionTimeGame.ino
+++ ReactionTimeGame/ReactionTimeGame.ino
@@ -5,15 +5,6 @@ const int green_led = 10;
 const int button = 1;
 const int empty_pin = A4;
 
-struct result {
-  int duration;
-  struct result *slower;
-}
-
-struct result *first = null;
-
-int fastest = 999999;
-
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
@@ -33,6 +24,14 @@ void push(int pin) {
 int randomInt(int min, int max) {
   return random() % (max - min + 1) + min;
 }
+
+struct result {
+  short reaction_time;
+  struct result *prev;
+  struct result *next;
+};
+
+struct result *first = NULL;
 
 void loop() {
   for (int i = 0; i < 3; i++) {
@@ -57,15 +56,45 @@ void loop() {
   unsigned long start = millis();
   push(button);
   unsigned long done = millis();
-  int reaction_time = done - start;
+  short reaction_time = done - start;
   lcd.clear();
   lcd.print("Reaction Time:");
   lcd.setCursor(0, 1);
   lcd.print(reaction_time);
   lcd.print(" ms.");
-  if (reaction_time < fastest) {
-    fastest = reaction_time;
-    lcd.print(" #1");
+
+  struct result *new_result = (result*)malloc(sizeof(struct result));
+  if (new_result == NULL) {
+    lcd.print("memory full");
+    exit(1);
   }
+  new_result->reaction_time = reaction_time;
+  new_result->prev = NULL;
+  new_result->next = NULL;
+  byte rank = 1;
+  if (first == NULL) {
+    first = new_result;
+  } else {
+    struct result *tmp = first;
+    do {
+      if (new_result->reaction_time < tmp->reaction_time) {
+        new_result->next = tmp;
+        new_result->prev = tmp->prev;
+        if (tmp->prev != NULL) {
+          tmp->prev->next = new_result;
+        }
+        tmp->prev = new_result;
+        break;
+      } else {
+        rank++;
+        tmp = tmp->next;
+      }
+    } while (tmp != NULL);
+    if (tmp == first) {
+      first = new_result;
+    }
+  }
+  lcd.print(" #");
+  lcd.print(rank);
   delay(1000);
 }
